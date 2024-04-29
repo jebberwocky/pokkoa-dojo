3c53633af9d581ef1f46015a4295fb73a1b7dc26
jeff <jebberwocky@gmail.com>
2024-04-29 16:11:38 +0800

    add DataScope - Aliyun to dojo

---
+++ datascope.py
@@ -0,0 +1,41 @@
+import random
+from http import HTTPStatus
+
+import dashscope
+from dashscope import Generation
+import yaml
+
+config = yaml.safe_load(open("dojo.yaml"))
+
+class PokkoaDataScope:
+    def __init__(self, model="qwen-turbo"):
+        self.model = model
+        self.model_namespace = "pokkoa.dojo.datascope"
+        self.system_rule = {"role": "system",
+                            "content": "你是一个只解释易经卦像的bot，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。"}
+        dashscope.api_key = config["datascope"]["apikey"]
+
+    def completion(self, prompt, temperature=0.3, top_p=1.0, presence_penalty=0, frequency_penalty=0):
+        if frequency_penalty < 0.0:
+            return " Repetition_penalty should be greater than 0.0"
+        print(self.model_namespace, " with model", self.model)
+        messages = [self.system_rule,
+                    {'role': 'user', 'content': prompt}]
+        response = Generation.call(
+            model=self.model,
+            messages=messages,
+            # 设置随机数种子seed，如果没有设置，则随机数种子默认为1234
+            seed=random.randint(1, 10000),
+            temperature=temperature,
+            top_p=top_p,
+            repetition_penalty=frequency_penalty,
+            # 将输出设置为"message"格式
+            result_format='message')
+        if response.status_code == HTTPStatus.OK:
+            return response.output.choices[0].message.content
+        else:
+            return ('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
+                response.request_id, response.status_code,
+                response.code, response.message
+            ))
+
--- dojo.py
+++ dojo.py
@@ -5,17 +5,25 @@ import sys
 
 from moonshot import Moonshot
 from pokkoagroq import PokkoaGroq
+from datascope import PokkoaDataScope
 
 # set up Moonshot
 moonshot = Moonshot()
 
 # set up Groq
-pokkoagroq = PokkoaGroq("llama3-8b-8192")
+pokkoa_llama3 = PokkoaGroq("llama3-8b-8192")
+pokkoa_mixtral = PokkoaGroq("mixtral-8x7b-32768")
+pokkoa_gemma = PokkoaGroq("gemma-7b-it")
+pokkoa_qwen = PokkoaDataScope("qwen-turbo")
 
 # models
-models = [moonshot,pokkoagroq]
+models = [moonshot,
+          pokkoa_llama3,
+          pokkoa_mixtral,
+          pokkoa_gemma,
+          pokkoa_qwen ]
 
-running_model = models[1]
+running_model = models[4]
 
 # set up the prompt
 with open("prompt.txt", "r") as file:
@@ -23,7 +31,7 @@ with open("prompt.txt", "r") as file:
 
 # set up the parameters
 temperature = [0.1, 0.3, 0.5, 1]
-top_p = [1.0]
+top_p = [0.8]
 presence_penalty = [2.0, 0, -2.0]
 frequency_penalty = [2.0, 0, -2.0]
 combinations = itertools.product(temperature, top_p, presence_penalty, frequency_penalty)
@@ -35,13 +43,16 @@ for temperature, top_p, presence_penalty, frequency_penalty in combinations:
     print(f"{running_model.model_namespace} with {temperature}, {top_p}, {presence_penalty}, {frequency_penalty}")
     start_time = time.time()
     content = running_model.completion(prompt,
-                                  temperature, top_p, presence_penalty, frequency_penalty)
+                                  temperature,
+                                       top_p,
+                                       presence_penalty,
+                                       frequency_penalty)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Execution time: {elapsed_time:.6f} seconds")
     print(content)
     output.append([temperature, top_p, presence_penalty, frequency_penalty, elapsed_time, content])
 
-with open('./out/'+str(pokkoagroq.model_namespace)+'.data_output.csv', 'w') as f:
+with open('./out/'+str(running_model.model_namespace)+'.'+running_model.model+'.data_output.csv', 'w') as f:
     write = csv.writer(f, quoting=csv.QUOTE_ALL)
     write.writerows(output)
